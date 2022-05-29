b795a5e00f9a42c33a295f61060b666df6d04d96
Topo <juanmatiasojeda@gmail.com>
2022-05-28 23:44:11 -0300

    Day 17 Finish

---
+++ Day017 - Quiz Game.py
@@ -0,0 +1,77 @@
+# question_data = [
+# {"text": "A slug's blood is green.", "answer": "True"},
+# {"text": "The loudest animal is the African Elephant.", "answer": "False"},
+# {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+# {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
+# {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
+# {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
+# {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+# {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+# {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+# {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+# {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+# {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+# ]
+
+# https://opentdb.com/api_config.php
+
+question_data = [
+    {
+        "category": "Animals",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "An octopus can fit through any hole larger than its beak.", "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+        "question": "The song 'Megalovania' by Toby Fox made its third appearence in the 2015 RPG 'Undertale'.", "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category":"General Knowledge","type":"boolean","difficulty":"easy","question":"French is an official language in Canada.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Art","type":"boolean","difficulty":"easy","question":"Leonardo da Vinci\'s Mona Lisa does not have eyebrows.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Entertainment: Video Games","type":"boolean","difficulty":"hard","question":"Unturned originally started as a Roblox game.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Entertainment: Music","type":"boolean","difficulty":"easy","question":"American rapper Dr. Dre actually has a Ph.D. doctorate.","correct_answer":"False","incorrect_answers":["True"]},{"category":"Entertainment: Comics","type":"boolean","difficulty":"hard","question":"In the comic book Archie, Betty is friends with Veronica because she is rich.","correct_answer":"False","incorrect_answers":["True"]},{"category":"History","type":"boolean","difficulty":"medium","question":"Sargon II, a king of the Neo-Assyrian Empire, was a direct descendant of Sargon of Akkad.","correct_answer":"False","incorrect_answers":["True"]},{"category":"Animals","type":"boolean","difficulty":"medium","question":"Kamea, the Gilbertese Islander word for dog, is derived from the English phrase Come here!","correct_answer":"True","incorrect_answers":["False"]},{"category":"General Knowledge","type":"boolean","difficulty":"easy","question":"Nutella is produced by the German company Ferrero.","correct_answer":"False","incorrect_answers":["True"]},{"category":"Entertainment: Video Games","type":"boolean","difficulty":"hard","question":"In the game Melty Blood Actress Again Current Code, you can enter Blood Heat mode in Half Moon style.","correct_answer":"False","incorrect_answers":["True"]},{"category":"Entertainment: Video Games","type":"boolean","difficulty":"easy","question":"There are 6 legendary cards in Clash Royale.","correct_answer":"False","incorrect_answers":["True"]},{"category":"Geography","type":"boolean","difficulty":"medium","question":"The Southeast Asian island of Borneo is politically divided among 3 countries.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Sports","type":"boolean","difficulty":"medium","question":"Skateboarding will be included in the 2020 Summer Olympics in Tokyo.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Entertainment: Video Games","type":"boolean","difficulty":"medium","question":"In World of Warcraft lore, Thrall is the original wielder of Doomhammer.","correct_answer":"False","incorrect_answers":["True"]},{"category":"Mythology","type":"boolean","difficulty":"medium","question":"According to Norse mythology, Loki is a mother.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Entertainment: Video Games","type":"boolean","difficulty":"easy","question":"Gordon Freeman, the protagonist of Half-Life, is said to have once had a ponytail.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Entertainment: Board Games","type":"boolean","difficulty":"easy","question":"The Angry Video Game Nerd\'s alter ego is Board James.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Animals","type":"boolean","difficulty":"easy","question":"Rabbits are rodents.","correct_answer":"False","incorrect_answers":["True"]},{"category":"Science: Computers","type":"boolean","difficulty":"easy","question":"In most programming languages, the operator ++ is equivalent to the statement += 1.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Entertainment: Music","type":"boolean","difficulty":"medium","question":"The Division Bell is the final album of the progressive rock band Pink Floyd.","correct_answer":"False","incorrect_answers":["True"]},{"category":"Entertainment: Film","type":"boolean","difficulty":"hard","question":"Cube, Cube 2: Hypercube and Cube Zero were directed by the same person.","correct_answer":"False","incorrect_answers":["True"]},{"category":"Entertainment: Japanese Anime & Manga","type":"boolean","difficulty":"easy","question":"In the Toaru Kagaku no Railgun anime,  espers can only reach a maximum of level 6 in their abilities.","correct_answer":"False","incorrect_answers":["True"]},{"category":"History","type":"boolean","difficulty":"hard","question":"The Battle of Trafalgar took place on October 23rd, 1805","correct_answer":"False","incorrect_answers":["True"]},{"category":"Vehicles","type":"boolean","difficulty":"medium","question":"Arriva is owned by the Deutsche Bahn.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Science: Computers","type":"boolean","difficulty":"easy","question":"The Windows ME operating system was released in the year 2000.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Politics","type":"boolean","difficulty":"hard","question":"Joko Widodo has appeared in the cover of a TIME magazine.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Geography","type":"boolean","difficulty":"easy","question":"California is larger than Japan.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Entertainment: Music","type":"boolean","difficulty":"medium","question":"For his performance at ComplexCon 2016 in Long Beach, California, Skrillex revived his Mothership set piece for one night only.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Entertainment: Music","type":"boolean","difficulty":"easy","question":"Daft Punk originated in France.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Entertainment: Film","type":"boolean","difficulty":"medium","question":"Joan Cusack starred in the 2009 disaster movie, 2012.","correct_answer":"False","incorrect_answers":["True"]},{"category":"Politics","type":"boolean","difficulty":"medium","question":"The State of Queensland, Australia voted NO to a referendum for daylight savings in 1992.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Entertainment: Japanese Anime & Manga","type":"boolean","difficulty":"hard","question":"Druid is a mage class in Log Horizon.","correct_answer":"False","incorrect_answers":["True"]},{"category":"Animals","type":"boolean","difficulty":"easy","question":"The Killer Whale is considered a type of dolphin.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Entertainment: Video Games","type":"boolean","difficulty":"medium","question":"In Super Mario World, the rhino mini-boss, Reznor, is named after the lead singer of the band Nine Inch Nails.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Science: Mathematics","type":"boolean","difficulty":"hard","question":"In Topology, the complement of an open set is a closed set.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Entertainment: Board Games","type":"boolean","difficulty":"medium","question":"Rich Uncle Pennybags from the board game Monopoly wears a monocle.","correct_answer":"False","incorrect_answers":["True"]},{"category":"Science: Mathematics","type":"boolean","difficulty":"hard","question":"L\'H&ocirc;pital was the mathematician who created the homonymous rule that uses derivatives to evaluate limits with indeterminations.","correct_answer":"False","incorrect_answers":["True"]},{"category":"General Knowledge","type":"boolean","difficulty":"medium","question":"Santa Claus is a variety of melon.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Geography","type":"boolean","difficulty":"easy","question":"Hungary is the only country in the world beginning with H.","correct_answer":"False","incorrect_answers":["True"]},{"category":"Entertainment: Japanese Anime & Manga","type":"boolean","difficulty":"hard","question":"In the To Love-Ru series, Peke is considered a female robot.","correct_answer":"True","incorrect_answers":["False"]},{"category":"General Knowledge","type":"boolean","difficulty":"easy","question":"Scotland voted to become an independent country during the referendum from September 2014.","correct_answer":"False","incorrect_answers":["True"]},{"category":"Science & Nature","type":"boolean","difficulty":"medium","question":"Pneumonoultramicroscopicsilicovolcanoconiosis is a synonym for the disease known as silicosis.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Entertainment: Video Games","type":"boolean","difficulty":"easy","question":"In the S.T.A.L.K.E.R. series, the Freedom faction wishes to destroy the supernatural area known as  the Zone.","correct_answer":"False","incorrect_answers":["True"]},{"category":"Entertainment: Music","type":"boolean","difficulty":"easy","question":"The 2011 movie The Adventures of Tintin was directed by Steven Spielberg.","correct_answer":"True","incorrect_answers":["False"]},{"category":"General Knowledge","type":"boolean","difficulty":"medium","question":"Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo. is a grammatically correct sentence.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Animals","type":"boolean","difficulty":"easy","question":"Rabbits are carnivores.","correct_answer":"False","incorrect_answers":["True"]},{"category":"Science & Nature","type":"boolean","difficulty":"medium","question":"Tachycardia or Tachyarrhythmia refers to a resting heart-rate near or over 100 BPM.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Entertainment: Film","type":"boolean","difficulty":"hard","question":"YouTube personality Jenna Marbles served as an executive producer of the film Maximum Ride (2016).","correct_answer":"True","incorrect_answers":["False"]},{"category":"General Knowledge","type":"boolean","difficulty":"easy","question":"The mitochondria is the powerhouse of the cell.","correct_answer":"True","incorrect_answers":["False"]}]
+
+class Question:
+
+    def __init__(self, text,answer) -> None:
+        self.text = text
+        self.answer = answer
+        
+class QuizBrain:
+
+    def __init__(self,question_list):
+        self.question_number = 0
+        self.score = 0
+        self.question_list = question_list
+        
+    def nextQuestion(self):
+        actualQuestion = self.question_list[self.question_number]
+        self.question_number+=1
+        resp = input(f"Q.{self.question_number}: {actualQuestion.text} (True/False)?: ")
+        self.check_answer(resp,actualQuestion.answer)
+
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
+
+    def check_answer(self, user_answer, correct_answer):
+        if (user_answer.lower() == correct_answer.lower()):
+            self.score +=1
+            print("You got it right!")
+        else:
+            print("Thats wrong.")
+        print(f"The correct answer is: {correct_answer}")
+        print(f"Your current score is: {self.score}/{self.question_number}")
+    
+    def final_score(self):
+        if (not self.still_has_questions):
+            print("You have completed the quizz")
+            print(f"Your Final Score is : {self.score}/{self.question_number}")
+
+
+if __name__ == "__main__":
+    questionBank = []
+    
+    for newQuestion in question_data:
+        questionBank.append(Question(newQuestion["question"],newQuestion["correct_answer"]))
+
+    quizgame = QuizBrain(questionBank)
+
+while (quizgame.still_has_questions()):
+    quizgame.nextQuestion()
+quizgame.final_score()
